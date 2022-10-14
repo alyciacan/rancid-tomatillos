8cc1a78127fc746ff3ad25a8b89be08fd396e313
Emily Miles <ms.emily.j.miles@gmail.com>
2022-10-14 19:38:08 -0400

    Add basic styling to MovieCard component

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,13 +1,12 @@
 import './MovieCard.css'
-import { Component } from 'react';
 
 const MovieCard = (props) => {
-    return (
-        <div className="movieCard">
-            <img src={`${ props.poster_path }`} />
-            <h2>{props.title}</h2>
-        </div>
-     )
+	return (
+		<div className="movieCard">
+			<img className="cardImage" src={props.poster_path} alt={`${props.title} Movie Poster`} />
+			<h2>{props.title}</h2>
+		</div>
+	 )
 }
 
 
