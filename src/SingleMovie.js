59d9061cd2dc8421596332e4bf249a6def5a9e9e
Emily Miles <ms.emily.j.miles@gmail.com>
2022-10-15 11:14:27 -0400

    Add basic css to SingleMovie component

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -3,22 +3,21 @@ import backArrow from './backArrow.png';
 
 const SingleMovie = (props) => {
     return (
-        <section className="single-movie">
-            <div className="movie-stats">
-                <img className="poster" src={props.singleMovie.poster_path} />
+        <section className='single-movie'>
+            <div className='movie-stats'>
+                <img className='poster' src={props.singleMovie.poster_path} alt={`${props.singleMovie.title} Movie Poster`} />
                 <h1>{props.singleMovie.title}</h1>
-                <h4>
-                    {props.singleMovie.release_date}   
-                 | {props.singleMovie.average_rating} 
+                <h4>{props.singleMovie.release_date}&nbsp;&nbsp; | &nbsp;&nbsp;{props.singleMovie.average_rating}</h4> 
                  {/* | {props.singleMovie.runtime} */}
-                 </h4>
             </div>
-            <div className="movie-details">
-                {/* <p>Description: {props.overview}</p> */}
+            <div className='movie-details'>
                 {/* <p>Genre: {props.genres}</p> */}
+                {/* <p>Description: {props.overview}</p> */}
                 {/* <p>Budget: ${props.budget}</p> */}
                 <p>Release Date: {props.singleMovie.release_date}</p>
-                <img src={backArrow} alt="Arrow to go back" onClick={ props.goBack } />
+                <div className='backArrow'>
+                    <img role='button' src={backArrow} alt='back button' width='75vw' height='75vh' onClick={ props.goBack } />
+                </div>
             </div>
         </section>
     )
